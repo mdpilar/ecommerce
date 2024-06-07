cde056e3a05eba72c3887eb213abec5ccc914019
mdpilar <154697211+mdpilar@users.noreply.github.com>
2024-06-07 13:13:13 -0300

    Add files via upload

---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,12 @@
+import { HiShoppingCart } from "react-icons/hi";
+
+const CartWidget = () => {
+    return (
+        <div> 
+            <HiShoppingCart />
+            <p>1</p>
+        </div> 
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import logoCyberLuv from "../../assets/logo.png"
+import CartWidget from "./CartWidget"
+import "./NavBar.css"
+const NavBar = () => {
+    return (
+        <><nav>
+            <div className="brand">
+                <img src={logoCyberLuv} alt="" />
+            </div>
+
+            <ul>
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>About</li>
+            </ul>
+        </nav><CartWidget /></>
+    ) 
+}
+
+export default NavBar 
